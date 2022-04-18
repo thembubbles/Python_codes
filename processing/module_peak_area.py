a0cd7f08c8377da6cc57e7f184635f8552218cc5
thembubbles <fe_prado_10@hotmail.com>
2022-04-18 09:33:20 +0200

    Revised processing scripts

--- processing/module_peak_area.py
+++ processing/module_peak_area.py
@@ -1,32 +1,58 @@
+# Author: Fernando Prado Araujo
+# with contributions from Thomas Van Gerve
+
 import numpy as np
 import pandas as pd
 
-import rampy as rp
 from sklearn.metrics import auc
+from csaps import csaps
 
-#import matplotlib.pyplot as plt
 
+# -------- Calculate Raman peak areas
 
 def peak_area(spectrum, file=None):
 
-    #get the raw values from the spectrum file
-    x = spectrum['x'].to_numpy()
+    #get the raw values from the spectrum file 
     
+    x = spectrum['x'].to_numpy()
+    y = spectrum['y'].to_numpy()
 
-    #separate the spectrum in dataframes for each gas peak
+    #separate the spectrum in multiple dataframes, one for each gas peak
     CO2_v1_peak = spectrum.query('1240 <= x <= 1300').copy()
     CO2_v2_peak = spectrum.query('1360 <= x <= 1420').copy()
     N2_peak = spectrum.query('2300 <= x <= 2345').copy()
     CH4_peak = spectrum.query('2890 <= x <= 2930').copy()
 
 
-    #set the regions of interest and attaching points for the local baseline corrections
-    bir_CO2_v1 = np.array([(1240,1260),(1290,1310)])
-    bir_CO2_v2 = np.array([(1360,1380),(1410,1430)])
-    bir_N2 = np.array([(2290,2310),(2330,2350)])
-    bir_CH4 = np.array([(2880,2900),(2920,2940)])
+    #set the baseline interpolation regions and attaching points for the local baseline corrections
+    bir1_CO2_v1 = [1240,1260]
+    bir2_CO2_v1 = [1290,1310]
+    bir1_CO2_v2 = [1360,1380]
+    bir2_CO2_v2 = [1410,1430]
+    bir1_N2 = [2290,2310]
+    bir2_N2 = [2330,2350]
+    bir1_CH4 = [2880,2900]
+    bir2_CH4 = [2920,2940]
+    
+    #create an array with all the interpolation regions and reshape the area to have fix size
+    birs = np.array(bir1_CO2_v1 + bir2_CO2_v1 + bir1_CO2_v2 + bir2_CO2_v2 + bir1_N2 + bir2_N2 + bir1_CH4 + bir2_CH4)
+    birs = birs.reshape(len(birs) // 2, 2)
 
-    #transform the dataframe columns into numpy array to allow baseline correction
+
+    #get the actual points in the spectrum by using the defined BIRS
+    spec_stack = np.column_stack((x, y))
+    for i, j in enumerate(birs):
+        if i == 0:
+            spectrumBir = spec_stack[(spec_stack[:, 0] > j[0]) & (spec_stack[:, 0] < j[1]), :]
+        else:
+            birRegion = spec_stack[(spec_stack[:, 0] > j[0]) & (spec_stack[:, 0] < j[1]), :]
+            # xfit= np.vstack((xfit,xtemp))
+            spectrumBir = np.row_stack((spectrumBir, birRegion))
+    
+    xbir, ybir = spectrumBir[:, 0], spectrumBir[:, 1]
+
+
+    #transform the dataframe columns into numpy arrays to allow baseline correction
     CO2_v1_x = CO2_v1_peak['x'].to_numpy()
     CO2_v1_y = CO2_v1_peak['y'].to_numpy()
     CO2_v2_x = CO2_v2_peak['x'].to_numpy()
@@ -37,88 +63,104 @@ def peak_area(spectrum, file=None):
     CH4_y = CH4_peak['y'].to_numpy()
 
 
-    #here the actual baseline removal takes place using the Rampy module
+    #here the actual baseline removal takes place using the csaps package
     #the IF conditional works to identify if the spectrum contains the specific wavenumbers of each gas
+    
+    spline = csaps(xbir, ybir, smooth=1e-4) # this line creates the spline function to fit the spectra using the previously defined BIRs
+    
     if CO2_v1_x.size > 0:
-        y_CO2_v1_PLS, back_CO2_v1 = rp.baseline(CO2_v1_x,CO2_v1_y,bir_CO2_v1,'drPLS',lam=10**5)
+        y_CO2_v1_PLS = spline(CO2_v1_x)
     else:
-        CO2_v1_peak['x'], y_CO2_v1_PLS, back_CO2_v1 = [0] * len(x), [0] * len(x), [0] * len(x)
+        CO2_v1_peak['x'], y_CO2_v1_PLS, CO2_v1_y = np.zeros(np.shape(x)), np.zeros(np.shape(x)), np.zeros(np.shape(x))
         print("the CO2 peak range was not measured for sample %s" %(file))
 
     if CO2_v2_x.size > 0:
-        y_CO2_v2_PLS, back_CO2_v2 = rp.baseline(CO2_v2_x,CO2_v2_y,bir_CO2_v2,'drPLS',lam=10**5)
+        y_CO2_v2_PLS = spline(CO2_v2_x)  
     else:
-        CO2_v2_peak['x'], y_CO2_v2_PLS, back_CO2_v2 = [0] * len(x), [0] * len(x), [0] * len(x)
+        CO2_v2_peak['x'], y_CO2_v2_PLS, CO2_v2_y = np.zeros(np.shape(x)), np.zeros(np.shape(x)), np.zeros(np.shape(x))
         print("the CO2 peak range was not measured for sample %s" %(file))
 
     if N2_x.size > 0:
-        y_N2_PLS, back_N2 = rp.baseline(N2_x,N2_y,bir_N2,'drPLS',lam=10**5)
+        y_N2_PLS = spline(N2_x) 
     else:
-        N2_peak['x'], y_N2_PLS, back_N2 = [0] * len(x), [0] * len(x), [0] * len(x)
+        N2_peak['x'], y_N2_PLS, N2_y = np.zeros(np.shape(x)), np.zeros(np.shape(x)), np.zeros(np.shape(x))
         print("the N2 peak range was not measured for sample %s" %(file))
 
     if CH4_x.size > 0:
-        y_CH4_PLS, back_CH4 = rp.baseline(CH4_x,CH4_y,bir_CH4,'drPLS',lam=10**5)
+        y_CH4_PLS = spline(CH4_x)
     else:
-        CH4_peak['x'], y_CH4_PLS, back_CH4 = [0] * len(x), [0] * len(x), [0] * len(x)
+        CH4_peak['x'], y_CH4_PLS, CH4_y = np.zeros(np.shape(x)), np.zeros(np.shape(x)), np.zeros(np.shape(x))
         print("the CH4 peak range was not measured for sample %s" %(file))
 
     
     #add the baseline corrected values to the peak dataframes
-    CO2_v1_peak['y_CO2_v1_PLS'] = y_CO2_v1_PLS
-    CO2_v2_peak['y_CO2_v2_PLS'] = y_CO2_v2_PLS
-    N2_peak['y_N2_PLS'] = y_N2_PLS
-    CH4_peak['y_CH4_PLS'] = y_CH4_PLS                    
-    CO2_v1_peak['back_CO2_v1'] = back_CO2_v1
-    CO2_v2_peak['back_CO2_v2'] = back_CO2_v2
-    N2_peak['back_N2'] = back_N2
-    CH4_peak['back_CH4'] = back_CH4        
+    CO2_v1_peak['y_CO2_v1_BL'] = CO2_v1_y - y_CO2_v1_PLS
+    CO2_v2_peak['y_CO2_v2_BL'] = CO2_v2_y - y_CO2_v2_PLS
+    N2_peak['y_N2_BL'] = N2_y - y_N2_PLS
+    CH4_peak['y_CH4_BL'] = CH4_y - y_CH4_PLS         
 
     #calculate the area under curve using sklearn module for each peak after baseline correction 
-    auc_CO2_v1 = auc(CO2_v1_peak['x'],CO2_v1_peak['y_CO2_v1_PLS'])
-    auc_CO2_v2 = auc(CO2_v2_peak['x'],CO2_v2_peak['y_CO2_v2_PLS'])
-    auc_N2 = auc(N2_peak['x'],N2_peak['y_N2_PLS'])
-    auc_CH4 = auc(CH4_peak['x'],CH4_peak['y_CH4_PLS'])
-
+    auc_CO2_v1 = auc(CO2_v1_peak['x'],CO2_v1_peak['y_CO2_v1_BL'])
+    auc_CO2_v2 = auc(CO2_v2_peak['x'],CO2_v2_peak['y_CO2_v2_BL'])
+    auc_N2 = auc(N2_peak['x'],N2_peak['y_N2_BL'])
+    auc_CH4 = auc(CH4_peak['x'],CH4_peak['y_CH4_BL'])
 
     dict = {'area_CO2_v1': auc_CO2_v1, 'area_CO2_v2': auc_CO2_v2, 'area_N2': auc_N2, 'area_CH4': auc_CH4} 
 
     df_peak_areas = pd.Series(dict).fillna(0)
 
     df_peak_areas[df_peak_areas < 0] = 0
-    
-    
-    
-    #plot peak features
-#     fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(16,5))
-
-#     ax1.plot(x,y,"b.",label="data")
-#     ax1.plot(CO2_v1_x,y_CO2_v1_PLS,"k-",label="model")
-#     ax1.plot(CO2_v1_x,back_CO2_v1,"r--",label="baseline")
-#     ax1.set_title('CO2_v1')
-#     ax1.set_xlim([1230,1310])
-#     ax2.plot(x,y,"b.",label="data")
-#     ax2.plot(CO2_v2_x,y_CO2_v2_PLS,"k-",label="model")
-#     ax2.plot(CO2_v2_x,back_CO2_v2,"r--",label="baseline")
-#     ax2.set_title('CO2_2v2')
-#     ax2.set_xlim([1350,1430])
-#     ax3.plot(x,y,"b.",label="data")
-#     ax3.plot(N2_x,y_N2_PLS,"k-",label="model")
-#     ax3.plot(N2_x,back_N2,"r--",label="baseline")
-#     ax3.set_title('N2_v1')
-#     ax3.set_xlim([2290,2360])
-#     ax4.plot(x,y,"b.",label="data")
-#     ax4.plot(CH4_x,y_CH4_PLS,"k-",label="model")
-#     ax4.plot(CH4_x,back_CH4,"r--",label="baseline")
-#     ax4.set_title('CH4_v1')
-#     ax4.set_xlim([2880,2940])
-
-#     ax1.set_ylim([0,500])
-#     ax2.set_ylim([0,500])
-#     ax3.set_ylim([0,500])
-#     ax4.set_ylim([0,4000])
-
-#     plt.legend()
-#     print(plt.show())
+   
     
     return df_peak_areas
+
+
+
+
+# -------- Calculate Raman cross-section
+
+def cross_section(gas, laser=532):
+        
+        # Reference values extracted from Burke (2001) Lithos, 
+        #dv is the gas Raman shift, 
+        #suma is relative normalized differential Raman scattering cross-sections
+    Raman_sigma = {'SO2' : {'dv' : 1151, "suma" : 4.03},
+                   'CO2_v1' : {'dv' : 1285, "suma" : 0.80},
+                   'CO2_2v2' : {'dv' : 1388, "suma" : 1.23},
+                   'O2' : {'dv' : 1555, "suma" : 1.03},
+                   'CO' : {'dv' : 2143, "suma" : 0.90},
+                   'N2' : {'dv' : 2331, "suma" : 1},
+                   'H2S' : {'dv' : 2611, "suma" : 6.8},
+                   'CH4' : {'dv' : 2917, "suma" : 8.63},
+                   'NH3' : {'dv' : 3336, "suma" : 6.32},
+                   'H2O' : {'dv' : 3657, "suma" : 3.29},
+                   'H2' : {'dv' : 4156, "suma" : 3.54}
+                  }
+
+    v0 = 10**7 / laser #convert laser wavelength from nm to cm-1
+
+    vi = Raman_sigma[gas]['dv']  #gas species Raman shift
+
+    h = 6.626*(10**(-27)) #Planck's constant [erg s]
+
+    c = 2.998*(10**(10)) #light velocity [cm/s]
+
+    k = 1.381*(10**(-16)) #Boltzmann’s constant [erg/K]
+
+    T = 273.15 + 25 #absolute temperature [K], is this case considered as room temperature at 25 oC
+
+    suma = Raman_sigma[gas]['suma'] #Suma values are relative normalized differential Raman scattering cross-sections
+
+# Suma and sigma are the different scattering values for a Raman shift vi (in cm), 
+# v0 is the laser wavelength used (in cm) (20 487, 19 435, and 15 802 for 488, 514, and 633 nm, respectively),
+# h is Planck’s constant 6.626 P 10 erg s , 
+# c is the light velocity 2.998 P 10 cm s , 
+# k is Boltzmann’s constant 1.381 P 10 erg K , 
+# and T is the absolute temperature.
+
+    cross_section = suma/((((v0-vi)**(-4))/((v0-2331)**(-4)))*(1-(np.exp((-h*c*vi)/(k*T)))))
+    #formula from Burke (2001) Lithos after Schrotter and ̈Klockner (1979)
+
+    cross_section = round(cross_section,1)
+
+    return cross_section
--- processing/processing_FI_Raman.py
+++ processing/processing_FI_Raman.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# **Raman data processing - Fluid inclusion vapor composition**
-#### This script reads the Raman spectra of fluid inclusions to measure the area under peaks and calculate the mol% of gas species present (for now only CO2, N2 and CH4).
+# # Raman spetroscopy data processing - composition of vapor phase in fluid inclusions
+# #### Calculate area under peak from Raman spectra and process to gas species mol%
 
-#*To ensure a smooth process, have all your files in the same folder. <br /> 
-#If you have vapor spectra files mixed with other files, make sure that all the vapor data files are in txt format and have the notation **"-v"** in the file name.*
+# ---------------- processing fluid inclusion Raman data (Raman spectroscopy)
 
-#Script written by [Fernando Araujo](https://github.com/thembubbles).
+print('This script works with text files containing Raman spectroscopy data of vapor phases in fluid inclusions.\nGas species molar fractions are calculated by the area under respective peaks.\n')
 
+print('Script made by Fernando Araujo, KU Leuven (fernando.pradoaraujo@kuleuven.be)')
 
-
-# ---------------- script for processing fluid inclusion Raman data (Raman spectroscopy)
-
-# ------- Calculate area under peak from Raman files and process to gas mol%
 
 # --- import modules
 
@@ -22,31 +18,58 @@ import glob
 import pandas as pd
 import numpy as np
 
-from tkinter import Tk, filedialog
-import warnings
 
-import rampy as rp
+import warnings
+from tkinter import Tk, filedialog
+
 from sklearn.metrics import auc
 
+from csaps import csaps
+
+
+# --- define functions to be used
 
 def peak_area(spectrum, file=None):
-
-    #get the raw values from the spectrum file
-    x = spectrum['x'].to_numpy()
+       
+    #get the raw values from the spectrum file 
     
+    x = spectrum['x'].to_numpy()
+    y = spectrum['y'].to_numpy()
 
-    #separate the spectrum in dataframes for each gas peak
+    #separate the spectrum in one dataframe for each gas peak
     CO2_v1_peak = spectrum.query('1240 <= x <= 1300').copy()
     CO2_v2_peak = spectrum.query('1360 <= x <= 1420').copy()
     N2_peak = spectrum.query('2300 <= x <= 2345').copy()
     CH4_peak = spectrum.query('2890 <= x <= 2930').copy()
 
 
-    #set the regions of interest and attaching points for the local baseline corrections
-    bir_CO2_v1 = np.array([(1240,1260),(1290,1310)])
-    bir_CO2_v2 = np.array([(1360,1380),(1410,1430)])
-    bir_N2 = np.array([(2290,2310),(2330,2350)])
-    bir_CH4 = np.array([(2880,2900),(2920,2940)])
+    #set the baseline interpolation regions and attaching points for the local baseline corrections
+    bir1_CO2_v1 = [1240,1260]
+    bir2_CO2_v1 = [1290,1310]
+    bir1_CO2_v2 = [1360,1380]
+    bir2_CO2_v2 = [1410,1430]
+    bir1_N2 = [2290,2310]
+    bir2_N2 = [2330,2350]
+    bir1_CH4 = [2880,2900]
+    bir2_CH4 = [2920,2940]
+    
+    #create an array with all the interpolation regions and reshape the area to have fixed size
+    birs = np.array(bir1_CO2_v1 + bir2_CO2_v1 + bir1_CO2_v2 + bir2_CO2_v2 + bir1_N2 + bir2_N2 + bir1_CH4 + bir2_CH4)
+    birs = birs.reshape(len(birs) // 2, 2)
+
+
+    #get the actual points in the spectrum by using the defined BIRS
+    spec_stack = np.column_stack((x, y))
+    for i, j in enumerate(birs):
+        if i == 0:
+            spectrumBir = spec_stack[(spec_stack[:, 0] > j[0]) & (spec_stack[:, 0] < j[1]), :]
+        else:
+            birRegion = spec_stack[(spec_stack[:, 0] > j[0]) & (spec_stack[:, 0] < j[1]), :]
+            # xfit= np.vstack((xfit,xtemp))
+            spectrumBir = np.row_stack((spectrumBir, birRegion))
+    
+    xbir, ybir = spectrumBir[:, 0], spectrumBir[:, 1]
+
 
     #transform the dataframe columns into numpy array to allow baseline correction
     CO2_v1_x = CO2_v1_peak['x'].to_numpy()
@@ -61,47 +84,45 @@ def peak_area(spectrum, file=None):
 
     #here the actual baseline removal takes place using the Rampy module
     #the IF conditional works to identify if the spectrum contains the specific wavenumbers of each gas
+    
+    spline = csaps(xbir, ybir, smooth=1e-4)
+    
     if CO2_v1_x.size > 0:
-        y_CO2_v1_PLS, back_CO2_v1 = rp.baseline(CO2_v1_x,CO2_v1_y,bir_CO2_v1,'drPLS',lam=10**5)
+        y_CO2_v1_PLS = spline(CO2_v1_x)
     else:
-        CO2_v1_peak['x'], y_CO2_v1_PLS, back_CO2_v1 = [0] * len(x), [0] * len(x), [0] * len(x)
+        CO2_v1_peak['x'], y_CO2_v1_PLS, CO2_v1_y = np.zeros(np.shape(x)), np.zeros(np.shape(x)), np.zeros(np.shape(x))
         print("the CO2 peak range was not measured for sample %s" %(file))
 
     if CO2_v2_x.size > 0:
-        y_CO2_v2_PLS, back_CO2_v2 = rp.baseline(CO2_v2_x,CO2_v2_y,bir_CO2_v2,'drPLS',lam=10**5)
+        y_CO2_v2_PLS = spline(CO2_v2_x)  
     else:
-        CO2_v2_peak['x'], y_CO2_v2_PLS, back_CO2_v2 = [0] * len(x), [0] * len(x), [0] * len(x)
+        CO2_v2_peak['x'], y_CO2_v2_PLS, CO2_v2_y = np.zeros(np.shape(x)), np.zeros(np.shape(x)), np.zeros(np.shape(x))
         print("the CO2 peak range was not measured for sample %s" %(file))
 
     if N2_x.size > 0:
-        y_N2_PLS, back_N2 = rp.baseline(N2_x,N2_y,bir_N2,'drPLS',lam=10**5)
+        y_N2_PLS = spline(N2_x) 
     else:
-        N2_peak['x'], y_N2_PLS, back_N2 = [0] * len(x), [0] * len(x), [0] * len(x)
+        N2_peak['x'], y_N2_PLS, N2_y = np.zeros(np.shape(x)), np.zeros(np.shape(x)), np.zeros(np.shape(x))
         print("the N2 peak range was not measured for sample %s" %(file))
 
     if CH4_x.size > 0:
-        y_CH4_PLS, back_CH4 = rp.baseline(CH4_x,CH4_y,bir_CH4,'drPLS',lam=10**5)
+        y_CH4_PLS = spline(CH4_x)
     else:
-        CH4_peak['x'], y_CH4_PLS, back_CH4 = [0] * len(x), [0] * len(x), [0] * len(x)
+        CH4_peak['x'], y_CH4_PLS, CH4_y = np.zeros(np.shape(x)), np.zeros(np.shape(x)), np.zeros(np.shape(x))
         print("the CH4 peak range was not measured for sample %s" %(file))
 
     
     #add the baseline corrected values to the peak dataframes
-    CO2_v1_peak['y_CO2_v1_PLS'] = y_CO2_v1_PLS
-    CO2_v2_peak['y_CO2_v2_PLS'] = y_CO2_v2_PLS
-    N2_peak['y_N2_PLS'] = y_N2_PLS
-    CH4_peak['y_CH4_PLS'] = y_CH4_PLS                    
-    CO2_v1_peak['back_CO2_v1'] = back_CO2_v1
-    CO2_v2_peak['back_CO2_v2'] = back_CO2_v2
-    N2_peak['back_N2'] = back_N2
-    CH4_peak['back_CH4'] = back_CH4        
+    CO2_v1_peak['y_CO2_v1_BL'] = CO2_v1_y - y_CO2_v1_PLS
+    CO2_v2_peak['y_CO2_v2_BL'] = CO2_v2_y - y_CO2_v2_PLS
+    N2_peak['y_N2_BL'] = N2_y - y_N2_PLS
+    CH4_peak['y_CH4_BL'] = CH4_y - y_CH4_PLS         
 
     #calculate the area under curve using sklearn module for each peak after baseline correction 
-    auc_CO2_v1 = auc(CO2_v1_peak['x'],CO2_v1_peak['y_CO2_v1_PLS'])
-    auc_CO2_v2 = auc(CO2_v2_peak['x'],CO2_v2_peak['y_CO2_v2_PLS'])
-    auc_N2 = auc(N2_peak['x'],N2_peak['y_N2_PLS'])
-    auc_CH4 = auc(CH4_peak['x'],CH4_peak['y_CH4_PLS'])
-
+    auc_CO2_v1 = auc(CO2_v1_peak['x'],CO2_v1_peak['y_CO2_v1_BL'])
+    auc_CO2_v2 = auc(CO2_v2_peak['x'],CO2_v2_peak['y_CO2_v2_BL'])
+    auc_N2 = auc(N2_peak['x'],N2_peak['y_N2_BL'])
+    auc_CH4 = auc(CH4_peak['x'],CH4_peak['y_CH4_BL'])
 
     dict = {'area_CO2_v1': auc_CO2_v1, 'area_CO2_v2': auc_CO2_v2, 'area_N2': auc_N2, 'area_CH4': auc_CH4} 
 
@@ -109,28 +130,84 @@ def peak_area(spectrum, file=None):
 
     df_peak_areas[df_peak_areas < 0] = 0
     
-    
-
     return df_peak_areas
 
 
 
-# --- set working directories
+def cross_section(gas, laser=532):
+        
+        # Reference values extracted from Burke (2001) Lithos, 
+        #dv is the gas Raman shift, 
+        #suma is relative normalized differential Raman scattering cross-sections
+    Raman_sigma = {'SO2' : {'dv' : 1151, "suma" : 4.03},
+                   'CO2_v1' : {'dv' : 1285, "suma" : 0.80},
+                   'CO2_2v2' : {'dv' : 1388, "suma" : 1.23},
+                   'O2' : {'dv' : 1555, "suma" : 1.03},
+                   'CO' : {'dv' : 2143, "suma" : 0.90},
+                   'N2' : {'dv' : 2331, "suma" : 1},
+                   'H2S' : {'dv' : 2611, "suma" : 6.8},
+                   'CH4' : {'dv' : 2917, "suma" : 8.63},
+                   'NH3' : {'dv' : 3336, "suma" : 6.32},
+                   'H2O' : {'dv' : 3657, "suma" : 3.29},
+                   'H2' : {'dv' : 4156, "suma" : 3.54}
+                  }
+
+    v0 = 10**7 / laser #convert laser wavelength from nm to cm-1
+
+    vi = Raman_sigma[gas]['dv']  #gas species Raman shift
+
+    h = 6.626*(10**(-27)) #Planck's constant [erg s]
+
+    c = 2.998*(10**(10)) #light velocity [cm/s]
+
+    k = 1.381*(10**(-16)) #Boltzmann’s constant [erg/K]
+
+    T = 273.15 + 25 #absolute temperature [K], is this case considered as room temperature at 25 oC
+
+    suma = Raman_sigma[gas]['suma'] #Suma values are relative normalized differential Raman scattering cross-sections
+
+# Suma and sigma are the different scattering values for a Raman shift vi (in cm), 
+# v0 is the laser wavelength used (in cm) (20 487, 19 435, and 15 802 for 488, 514, and 633 nm, respectively),
+# h is Planck’s constant 6.626 P 10 erg s , 
+# c is the light velocity 2.998 P 10 cm s , 
+# k is Boltzmann’s constant 1.381 P 10 erg K , 
+# and T is the absolute temperature.
+
+    cross_section = suma/((((v0-vi)**(-4))/((v0-2331)**(-4)))*(1-(np.exp((-h*c*vi)/(k*T)))))
+    #formula from Burke (2001) Lithos after Schrotter and ̈Klockner (1979)
+
+    cross_section = round(cross_section,1)
+
+    return cross_section
+
+
+
+
+# --- choose folder as working directory
 
 root = Tk() # pointing root to Tk() to use it as Tk() in program.
 root.withdraw() # Hides small tkinter window.
 root.attributes('-topmost', True) # Opened windows will be active. above all windows despite of selection.
 
+print('\n  Please select the folder that hosts the files \n')
 base_dir = filedialog.askdirectory()+'/'
 
+print(base_dir)
 
 # --- look into working directory and create a list with selected files - .txt
+
 os.chdir(base_dir)
 
-FI_files = glob.glob('*-v*.txt')
+entry = input("Please type the name of files to be moved (e.g., *-v*.txt):")
+
+FI_files = glob.glob(entry)
 
 
-df_Raman = pd.DataFrame(columns=['area_CO2_v1','area_CO2_v2','area_N2','area_CH4'])
+# print(FI_files)
+# print(len(FI_files))
+
+
+df_Raman = pd.DataFrame()
 
 
 np.seterr(divide='ignore', invalid='ignore')
@@ -138,7 +215,7 @@ warnings.filterwarnings('ignore')
 
 
 for file in FI_files:
-
+#     print(file)
     current_file = pd.read_csv(base_dir+file,
                     encoding = "ANSI", sep = '\t', names=('x','y'), comment="#")
 
@@ -158,48 +235,68 @@ df_Raman = df_Raman.fillna(0)
 
 
 
-Raman_cross_section = {'SO2' : 5.3,
-                       '12CO2_v1' : 1,
-                       '12CO2_2v2' : 1.5,
-                       '13CO2_2v2' :1.5,
-                       'O2' : 1.2,
-                       'CO' : 0.9,
-                       'N2' : 1,
-                       'H2S' : 6.4,
-                       'CH4' : 7.5,
-                       'NH3' : 5,
-                       'H2' : 2.3}
+#copy data from source dataframe
+df_V_mol = df_Raman.copy()
 
-
-df_V_mol = df_Raman[['Sample']].copy()
-
-
-df_V_mol['CO2_v1_mol'] = df_Raman['area_CO2_v1'] / Raman_cross_section['12CO2_v1']
-df_V_mol['CO2_v2_mol'] = df_Raman['area_CO2_v2'] / Raman_cross_section['12CO2_2v2']
-df_V_mol['N2_mol'] = df_Raman['area_N2'] / Raman_cross_section['N2']
-df_V_mol['CH4_mol'] = df_Raman['area_CH4'] / Raman_cross_section['CH4']
-
-
-df_V_mol['mol_sum'] = df_V_mol.sum(axis=1)
-
-df_Raman['X_CO2(mol%)'] = round(((df_V_mol['CO2_v1_mol']/df_V_mol['mol_sum']) + (df_V_mol['CO2_v2_mol']/df_V_mol['mol_sum'])),3)
-df_Raman['X_N2(mol%)'] = round((df_V_mol['N2_mol']/df_V_mol['mol_sum']),3)
-df_Raman['X_CH4(mol%)'] = round((df_V_mol['CH4_mol']/df_V_mol['mol_sum']),3)
-
-
-
-
-# df_Raman[['sample','piece','field','analysis','rest']
-#         ] = df_Raman["Sample"].str.split(pat='-', 
-#                                           n=4,  
-#                                           expand=True)
+#split source Sample columns into relevant columns
+df_V_mol[['sample','piece','field','analysis','rest']
+        ] = df_V_mol["Sample"].str.split(pat='-', 
+                                         n=4,  
+                                         expand=True)
                                          
+#adjust values format to fit with other datasets
+df_V_mol['piece'] = df_V_mol['piece'].str.upper()
+df_V_mol['field'] = df_V_mol['field'].astype(str)
+df_V_mol['analysis'] = df_V_mol['analysis'].astype(str)
 
-df_Raman = df_Raman[['Sample',
-                     'X_CO2(mol%)', 'X_N2(mol%)', 'X_CH4(mol%)',
-                     'area_CO2_v1', 'area_CO2_v2', 'area_N2', 'area_CH4']]
+df_V_mol['analysis'] = df_V_mol['analysis'].astype(str)
+
+#retrive laser wavelength from analysis name
+df_V_mol['laser'] = np.where(df_V_mol['rest'].str.contains('633', regex=True),
+                            633,
+                            np.where(df_V_mol['rest'].str.contains('785', regex=True),
+                                     785,
+                                     532)
+                           )
+
+#Here we calculate the Raman scattering cross-section, the function script is in the module_peak_area.py file
+df_V_mol['cross_section_CO2_v1'] = cross_section("CO2_v1",df_V_mol['laser'])
+df_V_mol['cross_section_CO2_v2'] = cross_section("CO2_2v2",df_V_mol['laser'])
+df_V_mol['cross_section_N2'] = cross_section("N2",df_V_mol['laser'])
+df_V_mol['cross_section_CH4'] = cross_section("CH4",df_V_mol['laser'])
+
+
+#Here the actual molar concentration calculation takes place - peak area divided by peak cross-section 
+df_V_mol['CO2_v1_mol'] = df_V_mol['area_CO2_v1'] / cross_section("CO2_v1",df_V_mol['laser'])
+df_V_mol['CO2_v2_mol'] = df_V_mol['area_CO2_v2'] / cross_section("CO2_2v2",df_V_mol['laser'])
+df_V_mol['N2_mol'] = df_V_mol['area_N2'] / cross_section("N2",df_V_mol['laser'])
+df_V_mol['CH4_mol'] = df_V_mol['area_CH4'] / cross_section("CH4",df_V_mol['laser'])
+
+#Sum each of the molar concentrations
+df_V_mol['mol_sum'] = df_V_mol['CO2_v1_mol'] + df_V_mol['CO2_v2_mol'] + df_V_mol['N2_mol'] + df_V_mol['CH4_mol'] 
+
+#molar pencertages from normalized molar concentrations
+df_V_mol['XCO2(mol%)'] = round(((df_V_mol['CO2_v1_mol']/df_V_mol['mol_sum']) + (df_V_mol['CO2_v2_mol']/df_V_mol['mol_sum'])),3)
+df_V_mol['XN2(mol%)'] = round((df_V_mol['N2_mol']/df_V_mol['mol_sum']),3)
+df_V_mol['XCH4(mol%)'] = round((df_V_mol['CH4_mol']/df_V_mol['mol_sum']),3)
 
 
 
-df_Raman.to_csv(base_dir+'FI_Raman_V_composition.csv',index=False)
-df_Raman
+
+df_FI_V = df_V_mol[['sample', 'piece', 'field', 'analysis',
+                     'XCO2(mol%)', 'XN2(mol%)', 'XCH4(mol%)',
+                     'area_CO2_v1', 'area_CO2_v2', 'area_N2', 'area_CH4', 
+                      'CO2_v1_mol', 'CO2_v2_mol', 'N2_mol', 'CH4_mol',
+                      'laser','rest'
+                      ,'cross_section_CO2_v1','cross_section_CO2_v2','cross_section_N2','cross_section_CH4'
+                     ]
+                    ]
+
+
+
+df_FI_V.to_csv('FI_Raman_V_composition.csv',index=False)
+df_FI_V
+
+
+
+
